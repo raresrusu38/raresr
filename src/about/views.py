af2feea77f2a01c838800f4d50a89bb8198f0b0a
raresrusu38 <rares.rusu29@gmail.com>
2020-03-23 18:15:26 +0200

    working on about page

--- src/about/views.py
+++ src/about/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect, HttpResponseRedirect
+from .models import AboutMe
 
-def about(request):
-    return render(request, 'about-us.html', {})
+def about(request, id=None):
+    queryset = get_object_or_404(AboutMe, id=1)
+    print(queryset)
+
+    context = {
+        'qs': queryset,
+    }
+
+    return render(request, 'about-us.html', context)
